5683b0eb6e35592098010d54e71329cde5e7a7fe
ntuvera <ntuvera@gmail.com>
2019-09-18 18:09:40 -0400

    I think this is more efficient, but leverages JS behavior of String + Number

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,21 +1,25 @@
-// for (let i = 0; i < 101; i++) {
-//   if (i % 3 == 0 && i % 5 == 0) {
-//     console.log('fizzbuzz');
-//   } else if (i % 5 == 0) {
-//     console.log('buzz');
-//   } else if (i % 3 == 0) {
-//     console.log('fizz');
-//   } else {
-//     console.log(i);
-//   }
+// for (let i = 1; i < 101; i++) {
+//   i % 3 == 0
+//     ? i % 5 == 0
+//       ? console.log('fizzbuzz')
+//       : console.log('fizz')
+//     : i % 5 == 0
+//       ? console.log('buzz')
+//       : console.log(i);
 // }
 
+// console.log('-----------');
+
 for (let i = 1; i < 101; i++) {
-  i % 3 == 0
-    ? i % 5 == 0
-      ? console.log('fizzbuzz')
-      : console.log('fizz')
-    : i % 5 == 0
-      ? console.log('buzz')
-      : console.log(i);
+  let string = '';
+  if (i % 3 == 0) {
+    string += 'fizz';
+  }
+  if (i % 5 == 0) {
+    string += 'buzz';
+  }
+  string = string ? string : i;
+
+  console.log(string);
 }
+console.log('-----------');
