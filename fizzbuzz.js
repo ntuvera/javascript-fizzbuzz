ef8d9a409726ded5c8cf12eac49431ee64438169
ntuvera <ntuvera@gmail.com>
2019-09-18 16:50:40 -0400

    having fun with ternary operators -- also fixed start count

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,21 @@
-for (let i = 0; i < 101; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
-    console.log('fizzbuzz');
-  } else if (i % 5 == 0) {
-    console.log('buzz');
-  } else if (i % 3 == 0) {
-    console.log('fizz');
-  } else {
-    console.log(i);
-  }
+// for (let i = 0; i < 101; i++) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log('fizzbuzz');
+//   } else if (i % 5 == 0) {
+//     console.log('buzz');
+//   } else if (i % 3 == 0) {
+//     console.log('fizz');
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+for (let i = 1; i < 101; i++) {
+  i % 3 == 0
+    ? i % 5 == 0
+      ? console.log('fizzbuzz')
+      : console.log('fizz')
+    : i % 5 == 0
+      ? console.log('buzz')
+      : console.log(i);
 }
